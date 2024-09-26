fcb53081c38d21d4632e0bd0a50b944fe09a031e
Guillaume HUSTA <guillaume.husta@gmail.com>
2024-09-26 13:09:18 +0200

    feat: Enable externalized properties with PropertySourcesPlaceholderConfigurer

--- src/main/java/org/example/config/BackendConfig.java
+++ src/main/java/org/example/config/BackendConfig.java
@@ -4,12 +4,15 @@ import com.zaxxer.hikari.HikariDataSource;
 import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.dialect.PostgreSQL10Dialect;
 import org.hibernate.tool.schema.Action;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.support.SharedEntityManagerBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -23,22 +26,42 @@ import java.util.Properties;
 @EnableTransactionManagement
 public class BackendConfig {
 
+    // Using relaxed binding for property names
+    // can also read env var 'JDBC_URL'
+    @Value("${jdbc-url:jdbc:postgresql://localhost:5432/world-db}")
+    private String propJdbcUrl;
+
+    @Value("${jdbc-username:world}")
+    private String propJdbcUsername;
+
+    @Value("${jdbc-password:world123}")
+    private String propJdbcPassword;
+
+    // Needed to resolve ${} placeholders in @Value annotations
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfig() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
     @Bean
     public DataSource dataSource() {
-        String jdbcUrl = "jdbc:postgresql://localhost:5432/world-db";
-        String user = "world";
-        String password = "world123";
+        String jdbcUrl = propJdbcUrl;
+        String user = propJdbcUsername;
+        String password = propJdbcPassword;
 
         HikariDataSource dataSource = new HikariDataSource();
         dataSource.setJdbcUrl(jdbcUrl);
         dataSource.setUsername(user);
         dataSource.setPassword(password);
+        // dataSource.setDriverClassName("org.postgresql.Driver");
         return dataSource;
     }
 
     @Bean
     public PlatformTransactionManager txManager(EntityManagerFactory emf) {
-        return new JpaTransactionManager(emf);
+        JpaTransactionManager jpaTransactionManager = new JpaTransactionManager(emf);
+        // jpaTransactionManager.setJpaDialect(new HibernateJpaDialect());
+        return jpaTransactionManager;
     }
 
     @Bean
