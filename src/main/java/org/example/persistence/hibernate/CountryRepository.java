aa733cbc4e9901b654cbcc118af9c5cf707af9b7
Guillaume HUSTA <guillaume.husta@gmail.com>
2024-10-05 09:48:33 +0200

    feat: Upgrade Spring FWK 6 + Hibernate 6

--- src/main/java/org/example/persistence/hibernate/CountryRepository.java
+++ src/main/java/org/example/persistence/hibernate/CountryRepository.java
@@ -5,8 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import java.util.List;
 import java.util.Optional;
 
