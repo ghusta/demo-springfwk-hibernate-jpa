be67df9d9aff586782d50aecad43d7146dd7ca3c
Guillaume HUSTA <guillaume.husta@gmail.com>
2024-11-04 08:55:26 +0100

    refactor: New @ExceptionHandler attributes in 6.2

--- src/main/java/org/example/exceptionhandling/CustomExceptionHandler.java
+++ src/main/java/org/example/exceptionhandling/CustomExceptionHandler.java
@@ -3,6 +3,7 @@ package org.example.exceptionhandling;
 import org.example.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -18,7 +19,7 @@ import java.util.Optional;
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(ResourceNotFoundException.class)
+    @ExceptionHandler(/* exception = ResourceNotFoundException.class,*/ produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Object> handleResourceNotFoundException(
             ResourceNotFoundException ex, WebRequest request) {
 
