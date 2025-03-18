8238dc1b8e46895e162fcde610313fdede14411f
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-03-18 11:31:48 +0100

    feat: Add some queries in CountryDataJpaRepository

--- src/main/java/org/example/persistence/data/jpa/CountryDataJpaRepository.java
+++ src/main/java/org/example/persistence/data/jpa/CountryDataJpaRepository.java
@@ -2,9 +2,19 @@ package org.example.persistence.data.jpa;
 
 import org.example.model.Country;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CountryDataJpaRepository extends JpaRepository<Country, String> {
+
+    List<Country> findAllByName(String name);
+
+    @Query("from Country where capital.name = :name")
+    List<Country> findAllByCapitalName(String name);
+
+    List<Country> findAllByNameContaining(String name);
 
 }
