6f91259f43a43b6d7d06f3c75a415064e7052b4f
Guillaume HUSTA <guillaume.husta@gmail.com>
2025-11-16 17:24:04 +0100

    fix: Manual migration for: Migrates from Jackson 2.x to Jackson 3.x + Spring Boot

--- src/main/java/org/example/config/JacksonConfig.java
+++ src/main/java/org/example/config/JacksonConfig.java
@@ -1,18 +1,27 @@
 package org.example.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.http.converter.json.JacksonJsonHttpMessageConverter;
+import tools.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import tools.jackson.databind.json.JsonMapper;
 
 @Configuration(proxyBeanMethods = false)
 public class JacksonConfig {
 
+//    @Bean
+//    public MappingJackson2HttpMessageConverter jsonConverter() {
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        // Customize objectMapper as needed
+//        return new MappingJackson2HttpMessageConverter(objectMapper);
+//    }
+
     @Bean
-    public MappingJackson2HttpMessageConverter jsonConverter() {
-        ObjectMapper objectMapper = new ObjectMapper();
+    public JacksonJsonHttpMessageConverter jsonConverter() {
+        JsonMapper objectMapper = JsonMapper.builder().build();
         // Customize objectMapper as needed
-        return new MappingJackson2HttpMessageConverter(objectMapper);
+        return new JacksonJsonHttpMessageConverter(objectMapper);
     }
 
 }
